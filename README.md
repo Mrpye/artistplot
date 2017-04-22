c1c4541fb2470425db5bb0b423f1fc7a5b658804
Andrew Pye <andrewp@metsi.co.uk>
2017-04-22 17:34:20 +0100

    Added photo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # artistplot
 Test project to see if can auto mate taking picture and plotting it
+
+
+#Install Pygame
+sudo pip install pygame
+
+#Install potrace
+
+apt-get install potrace
--- process.py
+++ process.py
@@ -1,5 +1,7 @@
 import pygame.camera
 import pygame.image
+from subprocess import call
+print "taking picture"
 
 pygame.camera.init()
 cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
@@ -8,3 +10,9 @@ cam.start()
 img = cam.get_image()
 pygame.image.save(img, "photo.bmp")
 pygame.camera.quit()
+
+print "Converting "
+call(["mkbitmap", "photo.bmp"])
+
+print "convert to svg "
+call(["sudo", "potrace","--svg","photo.pbm"])
