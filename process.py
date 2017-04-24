34dd14ff9bd072d7646a128058c77f98a482fbb3
DESKTOP-04SGI81\Andrewp <andrewp@metsi.co.uk>
2017-04-24 16:55:34 +0100

    Added test code

--- process.py
+++ process.py
@@ -1,38 +1,38 @@
-import pygame.camera
-import pygame.image
-from subprocess import call
+#import pygame.camera
+#import pygame.image
+#from subprocess import call
 import unicornlib
 import fileinput
-import serial
-import threading
+#import serial
+#import threading
 
 print "taking picture"
 
-pygame.camera.init()
-cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
-cam.start()
+#pygame.camera.init()
+#cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+#cam.start()
 
-img = cam.get_image()
-pygame.image.save(img, "photo.bmp")
-pygame.camera.quit()
+#img = cam.get_image()
+#pygame.image.save(img, "photo.bmp")
+#pygame.camera.quit()
 
-print "Converting "
-call(["mkbitmap", "photo.bmp"])
+#print "Converting "
+#call(["mkbitmap", "photo.bmp"])
 
-print "convert to svg "
-call(["potrace","--svg","-P 40mmx40mm","-W 40mm","-H40mm","--tight","photo.pbm"])
+#print "convert to svg "
+#call(["potrace","--svg","-P 40mmx40mm","-W 40mm","-H40mm","--tight","photo.pbm"])
 
 #code to remove the pt
-print "fix pt "
-f =fileinput.FileInput("photo.svg", inplace=True, backup='.bak')
-for line in f:
+#print "fix pt "
+#f =fileinput.FileInput("photo.svg", inplace=True, backup='.bak')
+#for line in f:
 	#print(line.replace('pt"', '"'))
-	if line.startswith("<g transform="):
-		print('<g transform="translate(50.000000,63.385773) scale(0.008858,-0.011925)"')
-	else:    
-		print(line.replace('pt"', '"'))
+	#if line.startswith("<g transform="):
+		#print('<g transform="translate(50.000000,63.385773) scale(0.008858,-0.011925)"')
+	#else:    
+		#print(line.replace('pt"', '"'))
 	
-f.close()
+#f.close()
 
 print "build gcode"
 e = unicornlib.MyEffect()
@@ -41,15 +41,15 @@ e.affect(['--tab="plotter_setup"', '--pen-up-angle=50', '--pen-down-angle=30', '
 file = open("photo.gcode","w") 
 
  
-for c in reversed(e.context.codes):
+for c in e.context.codes:
 	file.write(c + '\n') 
 
  
 file.close() 
 
-print "plot "
+#print "plot "
 #sudo chmod +x gctrl
-call(["/home/andrew/projects/artistplot/application.linux64/gctrl"])
+#call(["/home/andrew/projects/artistplot/application.linux64/gctrl"])
 
 
 
